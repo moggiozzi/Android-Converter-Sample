a35de2808069588fd35f350a7e6c0352a2e53267
moggiozzi <moggiozzi@gmail.com>
2013-12-03 23:34:55 +0400

    1) converting to both direction 2) delete button (handling text changing)

--- src/com/example/converter/MainActivity.java
+++ src/com/example/converter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.converter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -14,6 +15,40 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final EditText textBox1 = (EditText) findViewById( R.id.editText1 );
+		final EditText textBox2 = (EditText) findViewById( R.id.editText2 );
+		textBox1.addTextChangedListener(new TextWatcher(){
+	        public void afterTextChanged(Editable s) {
+	        	if ( !textBox1.isFocused() ) return;
+	        	String radStr = "NaN";
+	        	double grad = 0.0, rad = 0.0;
+	    		try{
+ 	  	          grad = Double.parseDouble(s.toString());
+ 	  	          rad = Math.toRadians(grad);
+ 	  	          radStr = Double.toString(rad);
+	    		}catch(NumberFormatException e){}
+	        	EditText textBox2 = (EditText) findViewById( R.id.editText2 );
+        		textBox2.setText(radStr);
+	        }
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+	        public void onTextChanged(CharSequence s, int start, int before, int count){}
+	    });
+		textBox2.addTextChangedListener(new TextWatcher(){
+	        public void afterTextChanged(Editable s) {
+	        	if ( !textBox2.isFocused() ) return;
+	        	String gradStr = "NaN";
+	        	double grad = 0.0, rad = 0.0;
+	    		try{
+ 	  	          rad = Double.parseDouble(s.toString());
+ 	  	          grad = Math.toDegrees(rad);
+ 	  	          gradStr = Double.toString(grad);
+	    		}catch(NumberFormatException e){}
+	        	EditText textBox1 = (EditText) findViewById( R.id.editText1 );
+         		textBox1.setText(gradStr);
+	        }
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+	        public void onTextChanged(CharSequence s, int start, int before, int count){}
+	    });
 	}
 
 	@Override
@@ -22,21 +57,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void doConvert(View view)
-	{
-		EditText textBox1 = (EditText) findViewById( R.id.editText1 );
-		EditText textBox2 = (EditText) findViewById( R.id.editText2 );
-		String strGrad = textBox1.getText().toString();
-		String strRad = "";
-		float grad = 0.0f;
-		float rad = 0.0f;
-		try{
-	      grad = Float.parseFloat(strGrad);
-		}
-		catch(NumberFormatException e){}
-		rad = grad * (float)Math.PI / 180.0f;
-        textBox2.setText(Float.toString(rad));
-	}
-
 }
